331bc02f19b9040758623db26d51ca7d92cade01
dungmai1985 <laviendevienne@gmail.com>
2019-09-21 19:06:50 +0700

    add homepage

---
+++ app/static/js/initFirebase.js
@@ -0,0 +1,13 @@
+// Script for configuring Firebase.
+// See https://firebase.google.com/docs/web/setup for more information.
+
+var config = {
+    apiKey: "AIzaSyD6gB4nj8lAIubBdMF95A07DrrC0-2XKJY",
+    authDomain: "book-recsys-253611.firebaseapp.com",
+    databaseURL: "https://book-recsys-253611.firebaseio.com",
+    projectId: "book-recsys-253611",
+    storageBucket: "book-recsys-253611.appspot.com",
+    messagingSenderId: "904167487951"
+};
+
+firebase.initializeApp(config);
